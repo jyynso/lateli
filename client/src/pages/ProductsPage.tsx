44d13c451329d2c891c9360572ac36393c8146fd
jyynso <ecwt26@gmail.com>
2026-07-15 10:34:25 +0800

    working on summer proejcts

--- client/src/pages/ProductsPage.tsx
+++ client/src/pages/ProductsPage.tsx
@@ -15,7 +15,7 @@ function ProductsPage() {
         const response = await fetch("http://localhost:3000/api/products");
         
         if(!response.ok) {
-          throw new Error("Failed to products")
+          throw new Error("Failed to fetch products")
         }
 
         const data = await response.json();
