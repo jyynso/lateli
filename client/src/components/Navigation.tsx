3b48546b4dd15708a6f18cd0586072a14391019a
jyynso <ecwt26@gmail.com>
2026-07-16 13:17:29 +0800

    style: make navigation fixed

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -4,21 +4,26 @@ import { Link } from 'react-router-dom';
 
 function Navigation() {
   return(
-  	<div className='flex items-center justify-between gap-10 p-3 pl-10 pr-10 bg-(--bg-card)'>
+  	<div className='fixed top-0 left-0 z-50 flex w-full items-center justify-between gap-10 p-3 pl-10 pr-10 bg-(--bg-card)'>
 			<div>
         <Link to={"/"} className='font-bold text-2xl'>lateli</Link>
 			</div>
 
     	<div className='flex items-center gap-10'>
 				<ul className='flex flex-row gap-10 mr-10'>
-					<Link to={"/"} className='font-semibold hover:underline'>home</Link>
-					<Link to={"/products"} className='font-semibold hover:underline'>artworks</Link>
+					<Link to={"/"} className='font-semibold hover:underline'>Home</Link>
+					<Link to={"/products"} className='font-semibold hover:underline'>Artworks</Link>
 				</ul>
 
-				<div className='flex flex-row gap-2 p-2 pl-2 pr-7 rounded-xs bg-white'>
-					<form>
+				<div className='flex flex-row gap-2 p-2 pl-2 pr-7 rounded-sm '>
+					<form className='flex items-center w-full'>
 						<MagnifyingGlassIcon size={25} />
-						<input type='text' placeholder='Search...' id='searchInput' />
+						<input 
+							type='text' 
+							placeholder='Search...' 
+							id='searchInput'
+							className='flex-1 pl-3 bg-transparent outline-none placeholder-gray-500'
+							/>
 					</form>
 				</div>
 				
--- client/src/pages/ProductsPage.tsx
+++ client/src/pages/ProductsPage.tsx
@@ -35,7 +35,7 @@ function ProductsPage() {
   console.log(user);
 
   return (
-    <div className='p-7'>
+    <div className='p-7 pt-15'>
       <SidebarFilter  />
       <div className='p-7 bg-(--bg-light) min-h-screen overflow-auto no-scrollbar'>
         <h1 className='font-bold text-4xl text-center mt-5'>Artworks</h1>
