a7875b734f64ee45b0d946770b12c3afe2774211
jyynso <ecwt26@gmail.com>
2026-07-17 14:28:11 +0800

    style: change modal width

--- client/src/components/SidebarCart.tsx
+++ client/src/components/SidebarCart.tsx
@@ -10,7 +10,7 @@ function SidebarCart() {
         onClick={() => setIsOpen(!isOpen)}
         aria-expanded={isOpen}
         aria-controls="sidebar-cart-panel"
-        className={`fixed z-50 top-20 shadow-lg ${isOpen ? "right-60" : "right-0"} p-1 rounded-l-md cursor-pointer bg-(--bg-white) transition-all duration-100`}>
+        className={`fixed z-50 top-20 shadow-lg ${isOpen ? "right-50" : "right-0"} p-1 rounded-l-md cursor-pointer bg-(--bg-white) transition-all duration-50`}>
         {isOpen ? (
           <ShoppingBagOpenIcon size={30} />
         ) : (
@@ -19,16 +19,18 @@ function SidebarCart() {
       </button>
 
       {isOpen && (
-        <div className="fixed inset-0 z-40" onClick={() => setIsOpen(false)}>
+        <div className="fixed inset-0 z-50" onClick={() => setIsOpen(false)}>
           <div
             id="sidebar-cart-panel"
             role="dialog"
             aria-modal="true"
             onClick={(e) => e.stopPropagation()}
-            className="fixed top-20 right-0 z-50 w-60 max-w-full rounded-bl-md bg-(--bg-white) p-4 shadow-lg">
-            <div className="">
+            className="fixed top-20 right-0 z-50 w-50 max-w-full rounded-bl-md bg-(--bg-white) p-4 shadow-lg">
+            <div className="flex flex-col gap-2">
               <h1 className="text-lg font-semibold">Cart</h1>
               <p>mcChicken</p>
+              <h1 className="text-lg font-semibold">Total</h1>
+
             </div>
           </div>
         </div>
