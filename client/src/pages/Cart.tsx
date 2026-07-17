84289ab58c96b13675901eb5c2883602bff55d75
jyynso <ecwt26@gmail.com>
2026-07-17 15:45:38 +0800

    style: add fix order summary and items container

--- client/src/pages/Cart.tsx
+++ client/src/pages/Cart.tsx
@@ -1,8 +1,16 @@
 function Cart() {
   return (
-    <div className="min-h-screen bg-(--bg-light) pt-24">
-      <div className="mx-auto max-w-6xl px-6 py-10">
-        <h1 className="text-center text-2xl font-semibold">wowzers</h1>
+    <div className="min-h-screen pt-24 bg-(--bg-light)">
+      <h1 className="text-center text-2xl font-semibold p-8">Your Cart</h1>
+
+      <div className="flex flex-row justify-center gap-10">
+        <div className="flex flex-col items-center p-10 rounded-md shadow-lg w-80  bg-(--bg-card)">
+          <h1 className="text-lg font-semibold">Items</h1>
+        </div>
+
+        <div className="flex flex-col items-center p-10 rounded-md shadow-lg w-60 bg-(--bg-card)">
+          <h1 className="text-lg font-semibold">Order Summary</h1>
+        </div>
       </div>
     </div>
   );
