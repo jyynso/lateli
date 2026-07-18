7c71509686770e5bbe880f1d2f22b50ef134ed13
jyynso <ecwt26@gmail.com>
2026-07-18 12:03:23 +0800

    style: add containers for better content coherence

--- client/src/pages/Cart.tsx
+++ client/src/pages/Cart.tsx
@@ -1,15 +1,20 @@
 function Cart() {
   return (
-    <div className="min-h-screen pt-24 bg-(--bg-light)">
-      <h1 className="text-center text-2xl font-semibold p-8">Your Cart</h1>
+    <div className="min-h-screen px-4 pt-24">
+      <h1 className="p-8 text-center text-2xl font-semibold">Your Cart</h1>
 
-      <div className="flex flex-row justify-center gap-10">
-        <div className="flex flex-col items-center p-10 rounded-md shadow-lg w-80  bg-(--bg-card)">
-          <h1 className="text-lg font-semibold">Items</h1>
+      <div className="mx-auto flex w-full max-w-5xl flex-col items-center justify-center gap-6 md:flex-row md:items-start">
+        <div className="flex w-full max-w-md flex-col items-center p-5">
+          <h1 className="text-xl font-semibold">Items</h1>
         </div>
 
-        <div className="flex flex-col items-center p-10 rounded-md shadow-lg w-60 bg-(--bg-card)">
-          <h1 className="text-lg font-semibold">Order Summary</h1>
+        <div className="flex w-full max-w-md h-50 flex-col gap-2 rounded-md bg-(--bg-card) p-5 shadow-sm">
+          <h1 className="text-xl font-semibold text-left">Order Summary</h1>
+          <p>Subtotal</p>
+          <p className="text-lg font-semibold">Total</p>
+          <button className="rounded-sm p-2.5 mt-auto bg-(--accent-charcoalBlue) text-(--bg-white)">
+            Checkout
+          </button>
         </div>
       </div>
     </div>
