bfb9006fa7b719e5b16a685606d5514fae7dd08f
jyynso <ecwt26@gmail.com>
2026-07-20 15:43:09 +0800

    feat: add toggleable function for opening sidebar filter

--- client/src/components/SidebarFilter.tsx
+++ client/src/components/SidebarFilter.tsx
@@ -4,34 +4,31 @@ import { useState } from "react";
 function SidebarCart() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const openFilter = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
     <div>
       <button
-        onClick={() => setIsOpen(!isOpen)}
+        onClick={openFilter}
         aria-expanded={isOpen}
         aria-controls="sidebar-cart-panel"
         className={`fixed z-50 top-30 shadow-md ${isOpen ? "left-50" : "left-0"} p-1 rounded-r-md cursor-pointer bg-(--bg-white) transition-all duration-50`}>
-        {isOpen ? (
-          <SlidersIcon size={30} />
-        ) : (
-          <SlidersIcon size={30} />
-        )}
+         <SlidersIcon size={30} /> 
       </button>
 
       {isOpen && (
-        <div className="fixed inset-0 z-50" onClick={() => setIsOpen(false)}>
-          <div
+        <div
             id="sidebar-cart-panel"
             role="dialog"
             aria-modal="true"
-            onClick={(e) => e.stopPropagation()}
             className="fixed top-20 left-0 z-50 w-50 h-110 p-4 max-w-full rounded-md shadow-md bg-(--bg-white)">
             <div className="flex flex-col gap-2">
               <h1 className="text-lg font-semibold">Filters</h1>
               <p>bababooey</p>
             </div>
           </div>
-        </div>
       )}
     </div>
   );
