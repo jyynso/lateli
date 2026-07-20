6d03b4a37b76a8fe7035e012da3287f8294afed7
jyynso <ecwt26@gmail.com>
2026-07-20 13:41:52 +0800

    style: change icon

--- client/src/components/SidebarFilter.tsx
+++ client/src/components/SidebarFilter.tsx
@@ -1,4 +1,4 @@
-import { ShoppingBagIcon, ShoppingBagOpenIcon } from "@phosphor-icons/react/dist/ssr";
+import { SlidersIcon } from "@phosphor-icons/react/dist/ssr";
 import { useState } from "react";
 
 function SidebarCart() {
@@ -12,9 +12,9 @@ function SidebarCart() {
         aria-controls="sidebar-cart-panel"
         className={`fixed z-50 top-20 shadow-md ${isOpen ? "left-50" : "left-0"} p-1 rounded-r-md cursor-pointer bg-(--bg-white) transition-all duration-50`}>
         {isOpen ? (
-          <ShoppingBagOpenIcon size={30} />
+          <SlidersIcon size={30} />
         ) : (
-          <ShoppingBagIcon size={30} />
+          <SlidersIcon size={30} />
         )}
       </button>
 
@@ -25,12 +25,10 @@ function SidebarCart() {
             role="dialog"
             aria-modal="true"
             onClick={(e) => e.stopPropagation()}
-            className="fixed top-20 left-0 z-50 w-50 max-w-full rounded-br-md bg-(--bg-white) p-4 shadow-lg">
+            className="fixed top-20 left-0 z-50 w-50 p-4 max-w-full rounded-br-md shadow-md bg-(--bg-white)">
             <div className="flex flex-col gap-2">
               <h1 className="text-lg font-semibold">Filters</h1>
-              <p>mcChicken</p>
-              <h1 className="text-lg font-semibold">Total</h1>
-
+              <p>bababooey</p>
             </div>
           </div>
         </div>
