517fa42de8ad1cb9806aaa7f76a876f509ae1686
jyynso <ecwt26@gmail.com>
2026-07-22 10:06:15 +0800

    fix: remove redundant component

--- client/src/pages/UploadArtworkPage.tsx
+++ client/src/pages/UploadArtworkPage.tsx
@@ -1,11 +1,12 @@
-
-
 function UploadArtwork() {
   return (
     <div className="min-h-screen pt-24">
       <h1 className="text-2xl font-bold text-center">Upload an Artwork</h1>
+      <div className="">
+        
+      </div>
     </div>
-  )
+  );
 }
 
 export default UploadArtwork;
