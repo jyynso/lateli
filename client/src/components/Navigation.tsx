122b4e30e1514fe553399c20662c49408ca69265
jyynso <ecwt26@gmail.com>
2026-08-03 12:09:53 +0800

    style: add conditional rendering for when the user is logged in or not

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -9,33 +9,26 @@ function Navigation() {
 
   return (
   	<div className='fixed top-0 left-0 z-50 flex flex-wrap w-full items-center justify-between gap-1 p-3 bg-(--bg-card)'>
-			<div>
-        <Link to={"/"} className='font-bold text-2xl'>lateli</Link>
-			</div>
+			<ul className='flex flex-row gap-5 items-center'>
+				<Link to={"/"} className='font-bold text-2xl'>lateli</Link>
+				<Link to={"/"} className='font-semibold hover:underline'>Home</Link>
+				<Link to={"/products"} className='font-semibold hover:underline'>Artworks</Link>
+			</ul>
+
+			<form className='flex items-center w-xs px-3 py-2 border-2 rounded-full'>
+				<input 
+					type='text' 
+					placeholder='Search...' 
+					id='searchInput'
+					className='flex-1 pl-3 bg-transparent outline-none w-full sm:w-30 placeholder-gray-500'
+					/>
+					<MagnifyingGlassIcon size={25} weight='bold' />
+			</form>
 
     	<div className='flex items-center gap-10'>
-				<ul className='flex flex-row gap-10 mr-10'>
-					<Link to={"/"} className='font-semibold hover:underline'>Home</Link>
-					<Link to={"/products"} className='font-semibold hover:underline'>Artworks</Link>
-				</ul>
-
-				<div className='flex flex-row gap-2 p-2 pl-2 pr-7 sm:pr-0  rounded-sm '>
-					<form className='flex items-center w-full'>
-						<MagnifyingGlassIcon size={25} />
-						<input 
-							type='text' 
-							placeholder='Search...' 
-							id='searchInput'
-							className='flex-1 pl-3 bg-transparent outline-none w-full sm:w-30 placeholder-gray-500'
-							/>
-					</form>
-				</div>
-				
 				<Link to={"/cart"}> <ShoppingCartSimpleIcon size={30}/> </Link>
 				<Link to={"/login"}> 
-					<UserCircleIcon
-  					size={30}
-  					color={isLoggedIn ? 'var(--accent-sandyBrown)' : 'var(--text-black)'}/>
+					{isLoggedIn ? <UserCircleIcon size={30}/> : "Sign in" }
 				</Link>
     	</div>
     </div>
