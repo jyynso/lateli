6133581803b098e6eb0026cbabd471e9fdde0053
jyynso <ecwt26@gmail.com>
2026-08-03 15:28:01 +0800

    style: add transition to the filter panel

--- client/src/components/Filter.tsx
+++ client/src/components/Filter.tsx
@@ -14,23 +14,26 @@ function Filter({className}: {className: string}) {
         onClick={openFilter}
         aria-expanded={isOpen}
         aria-controls="sidebar-cart-panel"
-        className={`flex items-center gap-1 rounded-full font-semibold text-sm bg-white px-3 py-2 shadow-md transition-all duration-50 ${className ?? ""}`}>
+        className={`flex items-center gap-1 rounded-full font-semibold text-sm bg-white px-3 py-2 shadow-md ${className ?? ""}`}>
          <SlidersHorizontalIcon size={18} weight="fill" /> 
          Filters
       </button>
 
-      {isOpen && (
-        <div className="fixed inset-0 z-40">
-          <div 
-            className="absolute inset-0 bg-black/40" 
-            onClick={() => setIsOpen(false)} />
-          <div className="absolute top-0 left-0 h-full w-80 max-w-[85vw] bg-(--bg-white) p-4 shadow-xl">
-            <h2 className="text-lg font-semibold">Filters</h2>
-            <p>Medium</p>
-            <p>Price</p>
-            </div>
+      <div
+        className={`fixed inset-0 z-40 transition-opacity duration-200 ${isOpen ? "pointer-events-auto opacity-100" : "pointer-events-none opacity-0"}`}
+      >
+        <div
+          className="absolute inset-0 bg-black/40"
+          onClick={() => setIsOpen(false)}
+        />
+        <div
+          className={`absolute top-0 left-0 h-full w-80 max-w-[85vw] bg-(--bg-white) p-4 shadow-xl transition-transform duration-300 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}
+        >
+          <h2 className="text-lg font-semibold">Filters</h2>
+          <p>Medium</p>
+          <p>Price</p>
         </div>
-)}
+      </div>
     </div>
   );
 }
