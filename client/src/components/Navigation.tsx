fa05c3736e0c13d90764d6c187f3cfa147545580
jyynso <ecwt26@gmail.com>
2026-08-03 12:31:12 +0800

    style: change order of search box on small screensn

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -8,19 +8,20 @@ function Navigation() {
 	const isLoggedIn = Boolean(user);
 
   return (
-  	<div className='fixed top-0 left-0 z-50 flex flex-wrap w-full items-center justify-between gap-1 p-3 bg-(--bg-card)'>
-			<ul className='flex flex-row gap-5 items-center'>
+  	<div className='sm:flex-row fixed top-0 left-0 z-50 flex flex-wrap w-full items-center justify-between gap-1 p-3 bg-(--bg-card)'>
+			
+			<ul className='flex flex-row gap-10 items-center px-2'>
 				<Link to={"/"} className='font-bold text-2xl'>lateli</Link>
 				<Link to={"/"} className='font-semibold hover:underline'>Home</Link>
 				<Link to={"/products"} className='font-semibold hover:underline'>Artworks</Link>
 			</ul>
 
-			<form className='flex items-center w-xs px-3 py-2 border-2 rounded-full'>
+			<form className='order-last w-full sm:order-0 sm:w-lg flex items-center px-3 py-2 border-2 rounded-full'>
 				<input 
 					type='text' 
 					placeholder='Search...' 
 					id='searchInput'
-					className='flex-1 pl-3 bg-transparent outline-none w-full sm:w-30 placeholder-gray-500'
+					className='flex-1 pl-3 bg-transparent outline-none w-full sm:w-30  placeholder-gray-500'
 					/>
 					<MagnifyingGlassIcon size={25} weight='bold' />
 			</form>
