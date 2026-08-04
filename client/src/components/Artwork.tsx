ae448c6a9bae2fb2b12d26892ae2f02ae4554a65
jyynso <ecwt26@gmail.com>
2026-08-04 16:31:06 +0800

    feat: add modal to view full artwork details

--- client/src/components/Artwork.tsx
+++ client/src/components/Artwork.tsx
@@ -1,4 +1,6 @@
 import { BasketIcon } from "@phosphor-icons/react/dist/ssr";
+import ViewCard from "./ViewArtworkCard";
+import { useState } from "react";
 
 type ArtworkProps = {
     image: string;
@@ -13,25 +15,35 @@ type ArtworkProps = {
 }
 
 function Artwork({ image, name, artist, description, medium, price, user, onAddToCart }: ArtworkProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <div className='flex flex-col group relative w-full lg:w-74 rounded-md transition-transform duration-200 hover:scale-103 hover:shadow-lg bg-(--bg-card) drop-shadow'>
-      <img src={image} alt={name} className='rounded-t-md h-70 object-cover'/>
-      <div className='flex flex-1 flex-col gap-1 p-5'>
-        <h1 className='text-xl font-semibold'>{name}</h1>
-        <p>Artist: {artist}</p>
-        <p className="line-clamp-2">Description: {description}</p>
-        <p>Medium: {medium}</p>
-        <p>Sold by: {user}</p>
-          <div className='mt-auto flex items-center justify-between'>
-            <p className='text-lg font-semibold'>₱ {price}</p>
-            <button 
-              onClick={onAddToCart} 
-              className='ml-auto rounded-full cursor-pointer p-2 shadow-md backdrop-blur-sm hover:bg-(--accent-sandyBrown) transition-all duration-50  bg-(--bg-white)'>
-              <BasketIcon size={30} />
-            </button>
-          </div>
+    <>
+      <div onClick={() => setIsModalOpen(true)} className='flex flex-col group relative w-full lg:w-74 rounded-md transition-transform duration-200 hover:scale-103 hover:shadow-lg bg-(--bg-card) drop-shadow'>
+        <img src={image} alt={name} className='rounded-t-md h-70 object-cover'/>
+        <div className='flex flex-1 flex-col gap-1 p-5'>
+          <h1 className='text-xl font-semibold'>{name}</h1>
+          <p>Artist: {artist}</p>
+          <p className="line-clamp-2">Description: {description}</p>
+          <p>Medium: {medium}</p>
+          <p>Sold by: {user}</p>
+            <div className='mt-auto flex items-center justify-between'>
+              <p className='text-lg font-semibold'>₱ {price}</p>
+              <button 
+                onClick={(e) => { 
+                  e.stopPropagation();
+                  onAddToCart}} 
+                className='ml-auto rounded-full cursor-pointer p-2 shadow-md backdrop-blur-sm hover:bg-(--accent-sandyBrown) transition-all duration-50  bg-(--bg-white)'>
+                <BasketIcon size={30} />
+              </button>
+            </div>
+        </div>
       </div>
-    </div>
+      <ViewCard 
+        artwork={{ imageUrl: image, name, artist, description, medium, price, user }} 
+        isOpen={isModalOpen} 
+        onClose={() => setIsModalOpen(false)} />
+    </>
   );
 }
 
--- client/src/components/ViewArtworkCard.tsx
+++ client/src/components/ViewArtworkCard.tsx
@@ -1,11 +1,55 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function ViewArtworkCard() {
-  const [isOpen, setIsOpen] = useState(false);
+type ViewArtworkCardProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  artwork: ArtworkData;
+};
+
+interface ArtworkData {
+  imageUrl: string;
+  name: string;
+  artist: string;
+  description: string;
+  medium: string;
+  price: number;
+  user: string;
+}
+
+export default function ViewArtworkCard({ isOpen, onClose, artwork }: ViewArtworkCardProps) {
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [artworks, setArtworks] = useState<ArtworkData[]>([]);
+  
+  useEffect(() => {
+    fetch('http://localhost:3000/api/artworks')
+      .then(res => res.json())
+      .then(data => {
+        setArtworks(data);
+        setLoading(false);
+      })
+      .catch(err => {
+        console.error(err);
+        setError("Failed to load artworks");
+        setLoading(false);
+      })
+    }, []);
+
+  if (!isOpen) return null;
 
   return (
-    <div className="hidden">
-
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50" onClick={onClose}>
+      <div className="bg-white flex flex-row rounded-md mx-5 w-full h-150 lg:w-4xl lg:h-120 relative" onClick={(e) => e.stopPropagation()}>
+          <img src={artwork.imageUrl} alt={artwork.name} className='rounded-l-md w-lg object-cover'/>
+          <div className="flex flex-col p-5 w-full bg-(--bg-card)">
+            <h1 className='text-xl font-semibold'>{artwork.name}</h1>
+            <p>Artist: {artwork.artist}</p>
+            <p className="line-clamp-2">Description: {artwork.description}</p>
+            <p>Medium: {artwork.medium}</p>
+            <p>Sold by: {artwork.user}</p>
+          </div>
+        </div>
     </div>
   );
 }
+
