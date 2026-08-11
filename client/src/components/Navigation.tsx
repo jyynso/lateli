d85db686d31f973af1ddd510a32625b527f1bb34
jyynso <ecwt26@gmail.com>
2026-08-11 11:35:59 +0800

    del: remove search bar and bg of navigation component

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -9,18 +9,8 @@ function Navigation() {
 	const isLoggedIn = Boolean(user);
 
   return (
-  	<div className='sm:flex-row fixed top-0 left-0 z-40 flex flex-wrap w-full items-center justify-between gap-1 p-3 bg-(--bg-card)'>
+  	<div className='sm:flex-row fixed top-0 left-0 z-40 flex flex-wrap w-full items-center justify-between gap-1 p-3'>
 			<Link to={"/"} className='font-bold text-2xl px-3'>lateli</Link>
-
-			<form className='order-last w-full sm:order-0 sm:w-lg flex items-center px-3 py-2 border-2 rounded-full'>
-				<input 
-					type='text' 
-					placeholder='Search...' 
-					id='searchInput'
-					className='flex-1 pl-3 bg-transparent outline-none w-full sm:w-30  placeholder-gray-500'
-					/>
-					<MagnifyingGlassIcon size={25} weight='bold' />
-			</form>
 
     	<div className='flex items-center gap-5 px-3'>
 				<Link to={"/products"} className='group relative'> 
