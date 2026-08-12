d2090fe61037baac19bc1db39fad315aea21cfc0
jyynso <ecwt26@gmail.com>
2026-08-12 14:41:47 +0800

    feat: add a dropdown on userCircleIcon onClick

--- client/src/components/AccountDropdown.tsx
+++ client/src/components/AccountDropdown.tsx
@@ -1,9 +1,28 @@
-
+import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import { SignOutIcon, TrayArrowUpIcon, UserCircleIcon } from "@phosphor-icons/react/dist/ssr";
 
 function AccountDropdown() {
+  const { user, logout } = useAuth();
+
   return (
-    <div>
-        
+    <div className="w-32 rounded-md bg-white p-4  shadow-lg transition-all duration-200 ease-out">
+      <span className="flex flex-row gap-2">
+        <UserCircleIcon size={26} weight="fill"/>
+        <p className="font-semibold">{user?.name}</p>
+      </span>
+      <ul className="mt-2 space-y-2 text-sm">
+        <li>
+          <Link to="/upload" className="flex flex-row gap-2 items-center font-semibold">
+          <TrayArrowUpIcon size={25} weight="fill" />
+          Upload
+          </Link>
+        </li>
+        <li className="flex flex-row gap-2 items-center font-semibold">
+          <SignOutIcon size={25} weight="fill"/> 
+          <button onClick={logout}>Logout</button>
+        </li>
+      </ul>
     </div>
   );
 }
--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,25 +1,56 @@
-import '/src/index.css'
+import { useState, useRef, useEffect } from 'react';
 import { UserCircleIcon, ImagesSquareIcon } from '@phosphor-icons/react/dist/ssr';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import Popup from './PopupText';
+import AccountDropdown from './AccountDropdown';
 
 function Navigation() {
 	const { user } = useAuth();
 	const isLoggedIn = Boolean(user);
+	const [menuOpen, setMenuOpen] = useState(false);
+	const menuRef = useRef<HTMLDivElement>(null);
+
+	  useEffect(() => {
+    function handleClickOutside(e: MouseEvent) {
+      if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
+        setMenuOpen(false);
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
 
   return (
   	<div className='sm:flex-row flex flex-wrap w-full items-center justify-between gap-1 p-3'>
-			<Link to={"/"} className='font-bold text-2xl px-3'>lateli</Link>
+			<Link to={"/"} className='font-serif text-2xl px-2 italic font-bold text-(--accent-charcoalBlue) whitespace-nowrap'>lateli</Link>
     	<div className='flex items-center gap-5 px-3'>
 				<Link to={"/products"} className='group relative'> 
 					<ImagesSquareIcon size={30} weight='fill' />
 					<Popup text='Artworks' className='top-full mt-2 text-sm'/> 
 				</Link>
-				<Link to={"/login"} className='text-sm font-semibold group relative'> 
-					{isLoggedIn ? <UserCircleIcon size={30} weight='fill'/> : "Sign in" }
-					<Popup className='top-full mt-2 text-sm' text={isLoggedIn ? "Account" : "Sign" } />
-				</Link>
+		 {isLoggedIn ? (
+        <div className="relative flex justify-center" ref={menuRef}>
+          <button
+            type="button"
+            onClick={() => setMenuOpen((prev) => !prev)}
+            className="group relative cursor-pointer"
+            aria-label="Open account menu" >
+              <UserCircleIcon size={30} weight="fill" />
+          </button>
+				  <div className={`absolute right-0 top-full mt-2 z-50 transition-all duration-150 ease-out origin-top-right
+      			${menuOpen 
+        			? 'opacity-100 scale-100 pointer-events-auto' 
+        			: 'opacity-0 scale-95 pointer-events-none'}`} >
+    					<AccountDropdown />
+  				</div>
+        </div>
+        ) : (
+          <Link to="/login" className="text-sm font-semibold group relative">
+            Sign in
+            <Popup className="top-full mt-2 text-sm" text="Sign" />
+          </Link>
+        )}
     	</div>
     </div>
     );
