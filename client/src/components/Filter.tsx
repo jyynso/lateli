7ae73b6d10beb5bc294679da0eed482f8b1d8bf3
jyynso <ecwt26@gmail.com>
2026-08-12 11:09:39 +0800

    style: change scaling for mobile

--- client/src/components/Filter.tsx
+++ client/src/components/Filter.tsx
@@ -22,9 +22,9 @@ function Filter({className}: {className: string}) {
       <div className={`fixed inset-0 z-40 transition-opacity duration-200 ${isOpen ? "pointer-events-auto opacity-100" : "pointer-events-none opacity-0"}`}>
         <div className="absolute inset-0 bg-black/40" onClick={() => setIsOpen(false)}/>
         <div className={`flex flex-col gap-2 absolute top-0 left-0 h-full w-80 max-w-[85vw] bg-(--bg-white) p-8 shadow-xl transition-transform duration-300 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
-          <h2 className="text-4xl font-semibold">Filters</h2>
-          <p>Medium</p>
-          <p>Price</p>
+          <h2 className="text-5xl lg:text-4xl font-semibold">Filters</h2>
+          <p className="text-xl lg:text-md">Medium</p>
+          <p className="text-xl lg:text-md">Price</p>
         </div>
       </div>
     </div>
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 
 function HomePage() {
   return (
-    <div className='flex flex-row justify-between p-20 h-screen'>
-      <div className='flex flex-col p-5 mt-10'>
+    <div className='flex flex-col lg:flex-row justify-none lg:justify-between p-6 lg:px-20 h-screen'>
+      <div className='flex flex-col mt-2 lg:mt-6'>
         <h1 className='font-bold text-5xl'>discover</h1>
         <h1 className='font-bold text-5xl'>arts</h1>
         <h1 className='font-bold text-[9rem]'>lateli</h1>
-        <Link to={"/products"} className='bg-(--accent-charcoalBlue) w-20 ml-auto text-center rounded-md text-white p-2'>shop</Link>
+        <Link to={"/products"} className='bg-(--accent-charcoalBlue) w-20 ml-auto text-center rounded-sm text-white p-1.5'>browse</Link>
       </div>
-      <div className='flex flex-row gap-8 p-5'>
-        <img className='h-70 mt-auto' src='../public/cafe.png' />
-        <img className='h-70' src='../public/duck.png' />
+      <div className='flex flex-col lg:flex-row gap-8 p-5'>
+        <img className='w-54 lg:h-70 lg:w-auto mt-1 mb-1 lg:mt-auto lg:mb-10' src='../public/cafe.png' />
+        <img className='w-48 lg:h-70 lg:w-auto ml-auto lg:ml-none' src='../public/duck.png' />
       </div>
     </div>
   );
