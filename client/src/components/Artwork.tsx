3a17618690fcd70d69129f90614740f91c94b911
jyynso <ecwt26@gmail.com>
2026-08-12 14:40:42 +0800

    del: remove details of the artwork in the productsPage to make the page much cleaner

--- client/src/components/Artwork.tsx
+++ client/src/components/Artwork.tsx
@@ -18,17 +18,16 @@ function Artwork({ image, name, artist, description, medium, price, user, showDe
 
   return (
     <>
-      <div onClick={() => setIsModalOpen(true)} className='flex flex-col group relative w-full lg:w-80'>
-        <img src={image} alt={name} className='h-74 object-cover transition-transform duration-200 hover:scale-103 hover:shadow-lg'/>
+      <div className='flex flex-col group relative w-full lg:w-80'>
+        <img src={image} alt={name} onClick={() => setIsModalOpen(true)} 
+          className='h-74 object-cover cursor-pointer transition-transform duration-200 hover:scale-103 hover:shadow-lg'/>
         <div className='flex flex-1 flex-col gap-1 py-3 px-1'>
-          <h1 className='text-xl font-semibold'>{name}</h1>
-          <p><span className="font-semibold">Artist:</span> {artist}</p>
+          {showDetails && description && <p>Name: {name}</p>}
+          {showDetails && description && <p>Artist: {artist}</p>}
           {showDetails && description && <p>Description: {description}</p>}
           {showDetails && medium && <p>Medium: {medium}</p>}
-          <p><span className="font-semibold">Listed by:</span> {user}</p>
-            <div className='mt-auto flex items-center justify-between'>
-              <p className='text-lg font-semibold'>₱{price}</p>
-            </div>
+          {showDetails && description && <p>Listed by: {user}</p>}
+          {showDetails && description && <p>Price: {price}</p>}
         </div>
       </div>
       <ViewCard 
