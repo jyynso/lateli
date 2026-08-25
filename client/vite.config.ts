8be9c75474b45ec697dd2eb05d1a5b64eb06d79f
jyynso <ecwt26@gmail.com>
2026-08-25 17:15:40 +0800

    feat: containerized project

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
@@ -15,6 +14,7 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+    port: 5174,
     host: true
   }
 })
